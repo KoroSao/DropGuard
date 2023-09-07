c61ad16485e8ddd26f79f7b30e94dbb51bce835c
KoroSao <rom1.froger@hotmail.fr>
2023-09-07 18:02:53 +0200

    API call and json parsing

--- popup.js
+++ popup.js
@@ -1,14 +1,27 @@
-
-// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     const activeTab = tabs[0];
-//     const tabUrl = activeTab.url;
-//     const urlElement = document.getElementById('tab-url');
-//     urlElement.textContent = tabUrl;
-// });
-
-
 // popup.js
-// popup.js
+
+
+function performGetRequest(url) {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json(); // Parse the JSON response
+    })
+    .then((data) => {
+      return data; // Return the parsed data
+    })
+    .catch((error) => {
+      throw new Error('Fetch error: ' + error);
+    });
+}
+
+
+
+
+
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   const tab = tabs[0];
   chrome.scripting.executeScript(
@@ -32,8 +45,42 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
           imgElement.src = imageUrl;
 
           // Append the image element to the popup
+          
           const popupDiv = document.getElementById('popup');
           popupDiv.appendChild(imgElement);
+          
+          
+          const requestUrl = 'https://serpapi.com/search.json?engine=google_lens&url=' + imageUrl;
+          // Call the performGetRequest function and store the result in a variable
+          const resultPromise = performGetRequest(requestUrl);
+
+          // Fetch the result from the promise and append it to the popup
+          resultPromise.then((result) => {
+            /*
+            const resultElement = document.createElement('p');
+            resultElement.textContent = JSON.stringify(result, null, 2);
+            popupDiv.appendChild(resultElement);
+            */
+            const images = result.visual_matches;
+            for (let i = 0; i < 15; i++) {
+              const imageElement = document.createElement('img');
+              imageElement.src = images[i].thumbnail;
+              popupDiv.appendChild(imageElement);
+              //Also add the source icon of the image
+              const sourceIcon = document.createElement('img');
+              sourceIcon.src = images[i].source_icon;
+              popupDiv.appendChild(sourceIcon);
+
+              // Also add the link of the source found in the link property
+              const linkElement = document.createElement('a');
+              linkElement.href = images[i].link;
+              linkElement.textContent = images[i].link;
+              popupDiv.appendChild(linkElement);
+            }
+          });
+          
+
+
         } else {
           // Create a "No product found" message
           const noProductMessage = document.createElement('p');
@@ -49,5 +96,6 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     }
   );
 });
+
 
 
